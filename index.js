bec9f193cae1d5f33a0afdc5919a015786b04bdf
joan <joan.kamau@student.moringaschool.com>
2024-07-19 16:44:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+// index.js
+
+function distanceFromHqInBlocks(location) {
+  
+    const headquarters = 42;
+   
+    return Math.abs(location - headquarters);
+}
+
+function distanceFromHqInFeet(location) {
+    const block = 264; // Each block is 264 feet
+    const blocks = distanceFromHqInBlocks(location); // Reusing the previously defined function
+    return blocks * block;
+}
+function distanceTravelledInFeet(start, destination) {
+    const block = 264; // Each block is 264 feet
+    const blocks = Math.abs(destination - start); // Calculate blocks travelled
+    return blocks * block; 
+}
+f
+    function calculatesFarePrice(start, destination) {
+        const distanceInFeet = distanceTravelledInFeet(start, destination);
+        const baseFare = 0;
+        const ratePerFoot = 0.02;
+   
+        } else if (distanceInFeet <= 2500) {
+          return flatFare; // Flat fare for distance between 2000 and 2500 feet
+        }
+        43-34*256
